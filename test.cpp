439344a4cfce710209a18d13c9e6bdf1f6002086
zhaofeng-shu33 <616545598@qq.com>
2019-11-10 16:56:19 +0800

    fix test api problem

--- test.cpp
+++ test.cpp
@@ -14,7 +14,8 @@ TEST(tc, io_bin) {
     ReadEdgesFromFile("test_io.bin", edges);
     uint64_t trcount = GpuForward(edges);
     EXPECT_EQ(trcount, 1); 
-    EXPECT_THROW(ReadEdgesFromFile("test_io_false.bin"),
+    edges.clear();
+    EXPECT_THROW(ReadEdgesFromFile("test_io_false.bin", edges),
                  std::logic_error);
 }
 
