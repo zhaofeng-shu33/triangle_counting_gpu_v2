636eed60102e909aa749044ffefd27eaabf162a3
zhiyuan <86562713@qq.com>
2019-11-10 20:03:02 +0800

    add v2

--- nvtc/gpu.h
+++ nvtc/gpu.h
@@ -5,6 +5,7 @@
 #include <stdint.h>
 
 uint64_t GpuForward(const Edges& edges);
+uint64_t GpuForward_v2(const Edges& edges);
 uint64_t MultiGpuForward(const Edges& edges, int device_count);
 
 void PreInitGpuContext(int device = 0);
