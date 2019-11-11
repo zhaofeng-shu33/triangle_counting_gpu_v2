bf28b55cab666005b02a0c507c03b47c29bf5491
zhaofeng-shu33 <616545598@qq.com>
2019-11-11 10:47:45 +0800

    remove use if else

--- nvtc/gpu.cu
+++ nvtc/gpu.cu
@@ -66,14 +66,15 @@ __global__ void CalculateTriangles_v2(int n, int* dev_neighbor, int64_t* dev_off
         int64_t i_it = dev_offset[i];
 	
  	while(dev_neighbor[j_it] <= dev_neighbor[dev_offset[j+1]-1] && dev_neighbor[i_it] <= dev_neighbor[dev_offset[i+1]-1]){
-		if (dev_neighbor[i_it] == dev_neighbor[j_it]){
+        int d = dev_neighbor[i_it] - dev_neighbor[j_it];
+		if ( d == 0 ){
 			count++;
 			i_it++;
 			j_it++;
 		}
-		else if (dev_neighbor[i_it] < dev_neighbor[j_it])
+		if (d < 0)
 			i_it++; 
-		else
+		if (d > 0)
 			j_it++;
 	}
      }
