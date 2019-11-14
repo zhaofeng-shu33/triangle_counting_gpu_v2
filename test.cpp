4be209170d67172d489ee17e737d3413923d96c6
zhaofeng-shu33 <616545598@qq.com>
2019-11-14 21:48:48 +0800

    fix test.cpp lint

--- test.cpp
+++ test.cpp
@@ -52,9 +52,10 @@ TEST(io, swap) {
 #if GPU
 TEST(tc, io_bin) {
     int* edges;
-    std::pair<int, uint64_t> info_pair = read_binfile_to_arclist("test_io.bin", edges);
+    std::pair<int, uint64_t> info_pair = read_binfile_to_arclist(
+        "test_io.bin", edges);
     uint64_t trcount = GpuForward(edges, info_pair.first, info_pair.second);
-    EXPECT_EQ(trcount, 1); 
+    EXPECT_EQ(trcount, 1);
     free(edges);
     EXPECT_THROW(read_binfile_to_arclist("test_io_false.bin", edges),
                  std::logic_error);
@@ -62,24 +63,29 @@ TEST(tc, io_bin) {
 
 TEST(tc, io_nvgraph) {
     int* edges;
-    std::pair<int, uint64_t> info_pair = read_binfile_to_arclist("test_io_nvgraph.bin", edges);
+    std::pair<int, uint64_t> info_pair = read_binfile_to_arclist(
+        "test_io_nvgraph.bin", edges);
     uint64_t trcount = GpuForward(edges, info_pair.first, info_pair.second);
-    EXPECT_EQ(trcount, 3); 
+    EXPECT_EQ(trcount, 3);
     free(edges);
 }
 
 TEST(split, io_nvgraph) {
     int* edges;
-    std::pair<int, uint64_t> info_pair = read_binfile_to_arclist("test_io_nvgraph.bin", edges);
-    uint64_t trcount = GpuForwardSplit(edges, info_pair.first, info_pair.second);
-    EXPECT_EQ(trcount, 3); 
+    std::pair<int, uint64_t> info_pair = read_binfile_to_arclist(
+        "test_io_nvgraph.bin", edges);
+    uint64_t trcount = GpuForwardSplit(edges,
+        info_pair.first, info_pair.second);
+    EXPECT_EQ(trcount, 3);
     free(edges);
 }
 TEST(split, io_complete_4) {
     int* edges;
-    std::pair<int, uint64_t> info_pair = read_binfile_to_arclist("test_complete_4.bin", edges);
-    uint64_t trcount = GpuForwardSplit(edges, info_pair.first, info_pair.second, 3);
-    EXPECT_EQ(trcount, 4); 
+    std::pair<int, uint64_t> info_pair = read_binfile_to_arclist(
+        "test_complete_4.bin", edges);
+    uint64_t trcount = GpuForwardSplit(edges,
+        info_pair.first, info_pair.second, 3);
+    EXPECT_EQ(trcount, 4);
     free(edges);
 }
 #if SECONDVERSION
@@ -99,17 +105,19 @@ TEST(tcv2, io_nvgraph) {
 
 TEST(cpu, io_bin) {
     int* arcs;
-    std::pair<int, uint64_t> info_pair = read_binfile_to_arclist("test_io.bin", arcs);
+    std::pair<int, uint64_t> info_pair = read_binfile_to_arclist(
+        "test_io.bin", arcs);
     uint64_t tcount = CpuForward(arcs, info_pair.first, info_pair.second);
-    EXPECT_EQ(tcount, 1);    
+    EXPECT_EQ(tcount, 1);
     free(arcs);
 }
 
 TEST(cpu, io_nvgraph) {
     int* arcs;
-    std::pair<int, uint64_t> info_pair = read_binfile_to_arclist("test_io_nvgraph.bin", arcs);
+    std::pair<int, uint64_t> info_pair = read_binfile_to_arclist(
+        "test_io_nvgraph.bin", arcs);
     uint64_t tcount = CpuForward(arcs, info_pair.first, info_pair.second);
-    EXPECT_EQ(tcount, 3);    
+    EXPECT_EQ(tcount, 3);
     free(arcs);
 }
 
