05c7e375ed74dc2a88c1ac0efd1f7254f5884cce
zhaofeng-shu33 <616545598@qq.com>
2019-11-20 09:37:34 +0800

    second version is the default

--- nvtc/main.cpp
+++ nvtc/main.cpp
@@ -21,26 +21,13 @@ int main(int argc, char *argv[]) {
 #if TIMECOUNTING 
     unique_ptr<Timer> t(Timer::NewTimer());
 #endif
-#if SECONDVERSION
     MyGraph myGraph(argv[2]);
-#else    
-    const char* io_hint = std::getenv("DATAIO");
-    const char* device_hint = std::getenv("DEVICEHINT");
-    int* edges;
-    std::pair<int, uint64_t> info_pair;
-    info_pair = read_binfile_to_arclist_v2(argv[2], edges);
-#if VERBOSE    
-    std::cout << "Num of Nodes: " << info_pair.first << std::endl;
-    std::cout << "Num of Edges: " << info_pair.second << std::endl;
-#endif
-#endif
 
 #if TRCOUNTING
     uint64_t result = 0;
 #if TIMECOUNTING
     t->Done("Reading Data");
 #endif
-#if SECONDVERSION
     int64_t cpu_split_target = (int64_t) ((double)(myGraph.offset[myGraph.nodeid_max+1]) * 0.02);
     // Disable cpu coorboration by set cpu_offset = 0;
     int64_t cpu_offset = 0;// *lower_bound(myGraph.offset,myGraph.offset+myGraph.nodeid_max+2,cpu_split_target);
@@ -58,21 +45,6 @@ int main(int argc, char *argv[]) {
     else{
         result = GpuForward_v2(myGraph);
     }
-#else
-#if GPU
-    if(device_hint == NULL || strcmp(device_hint, "GPU") == 0){
-       result = GpuForward(edges, info_pair.first, info_pair.second);
-    }
-    else if (strcmp(device_hint, "CPU") == 0) {
-        result = CpuForward(edges, info_pair.first, info_pair.second);
-    } else {
-        result = GpuForward(edges, info_pair.first, info_pair.second);
-    }
-#else
-	result = CpuForward(edges, info_pair.first, info_pair.second);
-#endif
-    free(edges);
-#endif
 #if TIMECOUNTING    
     t->Done("Compute number of triangles");
 #endif
